8d185531d92895649565bb810d1f60c3cf6eb0ad
AKRAM <loukrimyakram2@gmail.com>
2023-10-07 19:23:52 -0700

    Printing numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all single digit numbers of base 10 starting with 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
