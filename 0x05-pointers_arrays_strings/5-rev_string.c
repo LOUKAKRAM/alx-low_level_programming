1c8e313be667a23d74821d23705517345584fa5d
AKRAM <loukrimyakram2@gmail.com>
2023-10-18 14:31:41 -0700

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 {
 	int i, length = strlen(s), store;
 
-	for (i = 0; i < (length/2); i++)
+	for (i = 0; i < (length / 2); i++)
 	{
 		store = s[i];
 		s[i] = s[length - 1 - i];
